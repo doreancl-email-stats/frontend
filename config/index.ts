e8efd1c018623191d8e6f7f0c30f388d356205f1
doreancl <doreancl@gmail.com>
2022-11-04 08:54:31 -0300

    Cleanup for build

---
+++ config/index.ts
@@ -0,0 +1,5 @@
+export const API_URL = process.env.NEXT_PUBLIC_RECIPES_API_URL;
+export const BFF_API_URLa = process.env.NEXT_PUBLIC_BFF_API_URL;
+export const BFF_API_URL = '';
+export const clientId = process.env.GOOGLE_ID;
+export const clientSecret = process.env.GOOGLE_SECRET;
