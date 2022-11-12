3774d6d39d0bd2ad89450da98d644fb57c064d13
doreancl <doreancl@gmail.com>
2022-11-12 10:06:51 -0300

    fix async calls in useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,30 +2,44 @@ import "../styles/index.css";
 import type { AppProps } from "next/app";
 import { AppWrapper } from "../context/AppWrapper";
 import { SWRConfig } from "swr";
+import React from "react";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+import { BUGSNAG_APIKEY } from "../config";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Bugsnag.start({
+    apiKey: BUGSNAG_APIKEY,
+    plugins: [new BugsnagPluginReact()],
+  });
+
+  const ErrorBoundary = Bugsnag.getPlugin("react").createErrorBoundary(React);
+
   return (
-    <AppWrapper>
-      <SWRConfig
-        value={{
-          revalidateIfStale: false,
-          errorRetryCount: 1,
-          refreshInterval: 999999,
-          fetcher: (resource, init) =>
-            fetch(resource, init).then((res) => res.json()),
-        }}
-      >
-        <Component {...pageProps} />
-      </SWRConfig>
+    <ErrorBoundary>
+      <AppWrapper>
+        <SWRConfig
+          value={{
+            revalidateIfStale: false,
+            errorRetryCount: 1,
+            refreshInterval: 999999,
+            fetcher: (resource, init) =>
+              fetch(resource, init).then((res) => res.json()),
+          }}
+        >
+          <Component {...pageProps} />
+        </SWRConfig>
 
-      <style jsx global>{`
-        /* Other global styles such as 'html, body' etc... */
+        <style jsx global>{`
+          /* Other global styles such as 'html, body' etc... */
 
-        #__next {
-          height: 100%;
-        }
-      `}</style>
-    </AppWrapper>
+          #__next {
+            height: 100%;
+          }
+        `}</style>
+      </AppWrapper>
+    </ErrorBoundary>
+
     /*  <SWRConfig
           value={{
             fetcher: fetchJson,
